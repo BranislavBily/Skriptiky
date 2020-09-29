f01a97fd972c21b759edacb50894f9be789733d3
branislav's macbook pro <branislav.bily@gmail.com>
2020-09-29 12:06:41 +0200

    Ak je kniznica ako parameter tak ju prida, inak nie

--- compile.sh
+++ compile.sh
@@ -1,4 +1,7 @@
 
 #$1=file name
 #$2=library or something
-gcc -Wall -pedantic $1.c -o $1 -$2 && ./$1
+if [$2 == ""]
+	then gcc -Wall -pedantic $1.c -o $1 && ./$1
+else gcc -Wall -pedantic $1.c -o $1 -$2 && ./$1
+fi
