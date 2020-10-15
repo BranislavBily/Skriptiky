ae0c33f43cac30250f46fa29e891ba7a93cb569e
branislav's macbook pro <branislav.bily@gmail.com>
2020-10-15 17:54:08 +0200

    Pridanie optimalizacie

--- compile.sh
+++ compile.sh
@@ -2,6 +2,6 @@
 #$1=file name
 #$2=library or something
 if [$2 == ""]
-	then gcc -Wall -pedantic $1.c -o $1 && ./$1
+	then gcc -Wall -pedantic -O3 $1.c -o $1 && ./$1
 else gcc -Wall -pedantic $1.c -o $1 -$2 && ./$1
 fi
