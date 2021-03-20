b0b0f900f5ccd4e490572e5cd8731340709e3f7c
Branislav Bily <branislav.bily@gmail.com>
2021-03-20 21:18:08 +0100

    Kompilacia ako na progteste

--- compilep.sh
+++ compilep.sh
@@ -1,6 +1,2 @@
 #$1=file name
-#$2=library or something
-if [$2 == ""]
-	then g++ -Wall -pedantic -g $1.cpp -o $1 -fsanitize=address && ./$1
-else g++ -Wall -pedantic $1.cpp -o -g $1 -$2 -fsanitize=address && ./$1
-fi
+g++ -std=c++14 -Wall -pedantic -Wno-long-long -O2 -g $1.cpp -o $1 -fsanitize=address && ./$1
