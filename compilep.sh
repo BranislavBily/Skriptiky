7d2b6919819ec5e171ec93da0acda39e193b9042
Branislav Bily <branislav.bily@gmail.com>
2021-03-20 21:18:44 +0100

    Rychlejsia kompilacia > rychlejsi runtime

--- compilep.sh
+++ compilep.sh
@@ -1,2 +1,2 @@
 #$1=file name
-g++ -std=c++14 -Wall -pedantic -Wno-long-long -O2 -g $1.cpp -o $1 -fsanitize=address && ./$1
+g++ -std=c++14 -Wall -pedantic -Wno-long-long -g $1.cpp -o $1 -fsanitize=address && ./$1
